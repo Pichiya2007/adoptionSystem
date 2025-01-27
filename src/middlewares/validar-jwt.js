e416c94cc52b11da5e5e6cb4a1d15fe71b231ff8
Informatica <informatica@alumno.local>
2025-01-27 17:34:34 -0600

    Servidor creado

---
+++ src/middlewares/validar-jwt.js
@@ -0,0 +1,6 @@
+import jwt from 'jsonwebtoken';
+import Usuario from '../users/user.model.js';
+
+export const validarJWT = async (req, res, next) => {
+
+}
---
+++ src/users/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        require: [true, 'El nombre es obligatorio.']
+    },
+    correo: {
+        type: String,
+        require: [true, 'El correo es obligatorio.'],
+        unique: true
+    },
+    password: {
+        type: String,
+        require: [true, 'La contraseña es obligatoria.']
+    },
+    img: {
+        type: String
+    },
+    phone: {
+        type: String,
+        minLength: 8,
+        maxLength: 8,
+        require: true
+    },
+    role: {
+        type: String,
+        require: true,
+        enum: ['ADMIN ROLE', 'USER ROLE']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+UserSchema.methods.toJSON = function() {
+    const {__v, password, _id, ...usuario} = this.toObject();
+    usuario.uid = id;
+    return usuario;
+}
+
+export default mongoose.model('User', UserSchema);
